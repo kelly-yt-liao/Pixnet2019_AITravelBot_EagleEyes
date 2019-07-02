6d499c88c0a3504e06f93e74ab135d32aacdae16
Eason <leoling127@gmail.com>
2019-07-02 11:23:54 +0800

    mix html css

---
+++ main.js
@@ -0,0 +1,27 @@
+const endpoint = 'https://emma.pixnet.cc/mainpage/blog/categories/hot/28?api_version=2'
+const data = []
+
+fetch(endpoint)
+  .then(blob => blob.json())
+  .then(response => {
+    data.push(...response.articles)
+    displayData(data)
+  })
+
+const suggestion = document.querySelector('.suggestion')
+const button = document.querySelectorAll('button')
+
+function displayData(data) {
+  data.forEach(article => {
+    if (article.cover !== '') {
+      suggestion.innerHTML += `
+        <div class="suggest-item">
+          <a href="${article.link}" class="list" target="_blank">
+            <img class="cover"  src='${article.cover}' />
+            <div>${article.title}</div>
+          </a>
+        </div>
+      `
+    }
+  })
+}
