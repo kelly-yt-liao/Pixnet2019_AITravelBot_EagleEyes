85e4ca84c2910c58c55dc721acaf24e0d60a6515
Eason <leoling127@gmail.com>
2019-07-07 14:49:23 +0800

    change bootstrap to materialize

--- main.js
+++ main.js
@@ -25,3 +25,25 @@ fetch(endpoint)
     data.push(...response.articles)
     displayData(data)
   })
+
+const getNowDate = () => {
+  let nowDate = new Date()
+  return { nowYear: nowDate.getFullYear(), nowMonth: nowDate.getMonth(), nowDate: nowDate.getDate() }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  //下拉
+  const selectElems = document.querySelectorAll('select')
+  const selectInstances = M.FormSelect.init(selectElems, {})
+
+  //日期
+  const { nowYear, nowMonth, nowDate } = getNowDate()
+  const dateOptions = {
+    defaultDate: new Date(nowYear, nowMonth, nowDate),
+    setDefaultDate: true,
+    format: 'yyyy / mm / dd'
+  }
+  const dateElems = document.querySelectorAll('.datepicker')
+  const dateInstances = M.Datepicker.init(dateElems, dateOptions)
+  // dateInstances.setDate(new Date(nowYear, nowMonth, nowDate))
+})
